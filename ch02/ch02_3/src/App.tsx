4443695a59b9183ed541f843d7337a9019abd000
woojin <woojinchoi1023@gmail.com>
2024-06-22 21:44:35 +0900

    0622

--- ch02/ch02_3/src/App.tsx
+++ ch02/ch02_3/src/App.tsx
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import {Component} from 'react'
+// import ClassComponent from './ClassComponent'
 
-function App() {
+// export default class App extends Component {
+//   render() {
+//     return (
+//       <ul>
+//         <ClassComponent href="http://www.google.com" text="go to Google" />
+//         <ClassComponent href="http://www.twitter.com" text="go to twitter" />
+//       </ul>
+//     )
+//   }
+// }
+
+// export default function App() {
+//   return <div>Hello function-keyword component!</div>
+// }
+
+// const App = () => {
+//   return <h1>function component</h1>
+// }
+
+// export default App
+
+import ClassComponent from './ClassComponent'
+import ArrowComponent from './ArrowComponent'
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ul>
+      <ClassComponent href="http://www.google.com" text="go to Google" />
+      <ArrowComponent href="https://twitter.com" text="go to Twitter" />
+    </ul>
+  )
 }
-
-export default App;
---
+++ ch03/ch03_3/src/components/Div.tsx
@@ -0,0 +1,23 @@
+import type {
+  FC,
+  DetailedHTMLProps,
+  HTMLAttributes,
+  PropsWithChildren,
+} from "react";
+import type { WidthHeight } from "./WidthHeight";
+export type ReactDivProps = DetailedHTMLProps<
+  HTMLAttributes<HTMLDivElement>,
+  HTMLDivElement
+>;
+export type DivProps = ReactDivProps &
+  PropsWithChildren<WidthHeight> & {
+    src?: string;
+  };
+// prettier-ignore
+export const Div: FC<DivProps> = ({
+ width, height, style: _style, src, className: _className, ...props
+}) => {
+ const style = {..._style, width, height, backgroundImage: src && `url(${src})`}
+ const className = ['box-sizing', src && 'bg-gray-300', _className].join(' ')
+ return <div {...props} className={className} style={style} />
+}
