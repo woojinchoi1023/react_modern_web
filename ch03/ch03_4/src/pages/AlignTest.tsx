9abc5bab6f727daca2cd5d4b2910b5448f7bc97b
woojin <woojinchoi1023@gmail.com>
2024-06-26 09:53:32 +0900

    0625

---
+++ ch03/ch03_4/src/pages/AlignTest.tsx
@@ -0,0 +1,9 @@
+import { Title } from "../components";
+export default function CopyMe() {
+  return (
+    <section className="mt-4">
+      <Title>CopyMe</Title>
+      <div className="mt-4"></div>
+    </section>
+  );
+}
